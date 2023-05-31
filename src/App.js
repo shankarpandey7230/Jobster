d92b3ce86a41914db6823347f3e1c6238edd191e
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-31 12:20:08 +1000

    setting up project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Landing from "./pages/Landing";
+
+const App = () => {
+  return <Landing />;
+};
+
+export default App;
---
+++ src/pages/Landing.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+
+const Landing = () => {
+  return <h2>Landing</h2>;
+};
+
+export default Landing;

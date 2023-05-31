84022b6709b93f06108cd6e4409b4bc195053752
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-31 13:27:18 +1000

    setting up landing pages

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,35 @@
-import React from "react";
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 
 const Landing = () => {
-  return <h2>Landing</h2>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster-logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* {info} */}
+        <div className="info">
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            I'm baby four loko salvia etsy celiac mixtape. Banh mi normcore
+            kickstarter messenger bag. Adaptogen taxidermy fashion axe venmo.
+            Readymade lomo cred migas biodiesel slow-carb fixie. Tacos jawn
+            synth humblebrag DSA portland green juice marxism jianbing next
+            level 8-bit locavore. Man braid tacos scenester pok pok
+            chicharrones, hot chicken hell of. Vegan flexitarian blue bottle, yr
+            JOMO PBR&B whatever.
+          </p>
+          <button type="" className="btn btn-hero">
+            Login/Register
+          </button>
+        </div>
+        <img src={main} alt="job-hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;

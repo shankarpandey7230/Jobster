a8fd236599cdc4d2d2bb7fa952c7ec31d0350afc
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-06-03 00:13:57 +1000

    Css and React Icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Wrapper from "../assets/wrappers/Navbar";
+import { FaHome } from "react-icons/fa";
 
 const Navbar = () => {
-  return <h2>Navbar</h2>;
+  return (
+    <Wrapper>
+      <h2>Navbar</h2>
+      <FaHome />
+    </Wrapper>
+  );
 };
 
 export default Navbar;

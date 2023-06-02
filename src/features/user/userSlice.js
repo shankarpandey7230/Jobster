868823de7e24e34cbea23065e74d97e14ca04a6b
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-06-03 04:18:46 +1000

    refactoring userslice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,6 +6,11 @@ import {
   getUserFromLocalStorage,
   removeUserFromLocalStorage,
 } from "../../utils/localStorage";
+import {
+  loginUserThunk,
+  registerUserThunk,
+  updateUserThunk,
+} from "./userThunk";
 
 const initialState = {
   isLoading: false,
@@ -16,40 +21,20 @@ const initialState = {
 export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.post("/auth/register", user);
-      return resp.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
+    return registerUserThunk("/auth/register", user, thunkAPI);
   }
 );
 export const loginUser = createAsyncThunk(
   "user/loginUser",
   async (user, thunkAPI) => {
     // console.log(`Login User:${JSON.stringify(user)}`);
-    try {
-      const resp = await customFetch.post("/auth/login", user);
-      return resp.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
+    return loginUserThunk("/auth/login", user, thunkAPI);
   }
 );
 export const updateUser = createAsyncThunk(
   "user.updateUser",
   async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.patch("/auth/updateUser", user, {
-        headers: {
-          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-        },
-      });
-      return resp.data;
-    } catch (error) {
-      console.log(error.response);
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
+    return updateUserThunk("/auth/updateUser", user, thunkAPI);
   }
 );
 

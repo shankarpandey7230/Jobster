da3cfa9adb88d93d0bdaf275fd8a7cbc28206134
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-06-03 14:53:34 +1000

    creating jobslice

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import jobSlice from "./features/job/jobSlice";
 
 import userSlice from "./features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
+    job: jobSlice,
   },
 });
